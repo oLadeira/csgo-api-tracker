dfb8f7698476b14028a4c5b12531eafc66eb8f3b
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-28 15:14:09 -0300

    initial commit

---
+++ src/main/java/br/com/lucasladeira/csgoapitracker/CsgoApiTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.csgoapitracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgoApiTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsgoApiTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasladeira/csgoapitracker/CsgoApiTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.csgoapitracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgoApiTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
